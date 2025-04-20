32db52d3af86968248530a1cb3b0c071686b4b08
DUCMINN <taminhduc765@gmail.com>
2025-04-20 20:58:58 +0700

    Update Resource.h

--- Resource.h
+++ Resource.h
@@ -1,6 +1,12 @@
-#pragma once
+#ifndef RESOURCES_H
+#define RESOURCES_H
+
 #include <SFML/Graphics.hpp>
-#include <string>
+#include <SFML/Audio.hpp>
 
 bool loadTexture(sf::Texture& texture, const std::string& filepath);
 bool loadFont(sf::Font& font, const std::string& filepath);
+std::vector<int> loadHighScores(const std::string& filename);
+void saveHighScore(const std::string& filename, int score);
+
+#endif // RESOURCES_H

1f684adda611514eb2438669012967efa9938dc1
DUCMINN <taminhduc765@gmail.com>
2025-04-20 23:56:38 +0700

    Update Resource.h

--- Resource.h
+++ Resource.h
@@ -1,12 +1,23 @@
+// Resources.h
 #ifndef RESOURCES_H
 #define RESOURCES_H
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <map>
+#include <string>
 
-bool loadTexture(sf::Texture& texture, const std::string& filepath);
-bool loadFont(sf::Font& font, const std::string& filepath);
-std::vector<int> loadHighScores(const std::string& filename);
-void saveHighScore(const std::string& filename, int score);
+class Resources {
+public:
+    static void load();  // Gọi một lần khi khởi tạo
+    static const sf::Texture& getTexture(const std::string& name);
+    static const sf::Font& getFont(const std::string& name);
+    static const sf::SoundBuffer& getSound(const std::string& name);
 
-#endif // RESOURCES_H
+private:
+    static std::map<std::string, sf::Texture> textures;
+    static std::map<std::string, sf::Font> fonts;
+    static std::map<std::string, sf::SoundBuffer> sounds;
+};
+
+#endif

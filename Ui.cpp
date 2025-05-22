8db36f108e5c61685694afa7d2b9d7a1173ced1a
DUCMINN <taminhduc765@gmail.com>
2025-05-22 20:08:38 +0700

    Delete Ui.cpp

--- Ui.cpp
+++
@@ -1,7 +0,0 @@
-#include "UI.h"
-
-void renderUI(sf::RenderWindow& app, sf::Sprite& hud, sf::Text& timeText, sf::Text& scoreText) {
-    app.draw(hud);
-    app.draw(timeText);
-    app.draw(scoreText);
-}
